0344a09b88d8dea000973071cae871f2c090a40b
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-09-29 11:53:23 +0200

    feat: fetched movies and actors from api for home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,26 @@
 import { ref, onMounted } from 'vue'
 import axios from 'axios'
 
-const data = ref([]);
+const moviesData = ref([]);
+const actorsData = ref([]);
 onMounted(async () => {
-  const response = await axios.get('https://127.0.0.1:8000/api/movies?page=1')
-  data.value = response.data
+  const moviesResponse = await axios.get('https://127.0.0.1:8000/api/movies?page=1')
+  moviesData.value = moviesResponse.data
+  console.log(moviesData.value)
+  const actorsResponse = await axios.get('https://127.0.0.1:8000/api/actors?page=1')
+  actorsData.value = actorsResponse.data
 })
-console.log(data.value)
 </script>
 
 <template>
-  <h1>Hello World</h1>
-  <div v-for="movie in data['hydra:member']">
-    <div v-for="n in 5">
-      <h1>{{ movie.title }}</h1>
-      <p>{{ movie.description }}</p>
-    </div>
+  <div v-if="moviesData['hydra:member']" v-for="movie in moviesData['hydra:member'].slice(0,4)">
+    <img src="" alt="">
+    <h2>{{ movie.title }}</h2>
+    <p>{{ movie.description }}</p>
+  </div>
+  <hr>
+  <div v-if="actorsData['hydra:member']" v-for="actor in actorsData['hydra:member'].slice(0,4)">
+    <h2>{{ actor.firstName }} {{ actor.lastName }}</h2>
+    <p>{{ actor.description }}</p>
   </div>
 </template>
