0e273c0ace5b209265e922e3e6336b5fe2c4c176
Maxime Fourna <65597525+maxfrn65@users.noreply.github.com>
2023-11-24 11:57:16 +0100

    feat: search form

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,9 +1,35 @@
 <script setup>
+import axios from 'axios'
+import {ref} from "vue";
 
+let token = localStorage.getItem('token');
+let searchResults = ref('');
+let searchInput = ref('');
+const search = async () => {
+  try {
+    setTimeout( async () => {
+      const response = await axios.get(`https://127.0.0.1:8000/api/movies?title=${searchInput.value}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+        }
+
+      })
+      searchResults.value = response.data
+    console.log(searchResults);
+  }, 1500)} catch (error) {
+    console.log(error)
+  }
+}
 </script>
 
 <template>
-  <input type="text" name="search" placeholder="Search...">
+  <input type="text" name="search" placeholder="Search..." @input="search" v-model="searchInput">
+  <div>
+    <div v-if="searchInput" v-for="result in searchResults['hydra:member']">
+      <h1>{{ result.title }}</h1>
+      <p>{{ result.description }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
